4ebd904060a9792336bce33d0b00345aa595dc3e
tlelks <47315424+tlelks@users.noreply.github.com>
2019-10-23 22:08:58 +0700

    setup + Ex4

---
+++ Exercise4_Laksup_S.py
@@ -0,0 +1,9 @@
+fe = 84
+gb = 74.5
+ics = 86.75
+cp = 90
+print("--- Your Score ---")
+print("Foundation English               :",fe)
+print("General Business                 :",gb)
+print("Introduction to Computer Systems :",ics)
+print("Computer Programming             :",cp)
