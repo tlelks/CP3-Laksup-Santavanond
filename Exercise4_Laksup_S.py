a5dcbbbf712d8265c7211fb74b4aff1910243d86
tlelks <47315424+tlelks@users.noreply.github.com>
2019-10-24 15:12:44 +0700

    clear blank line

--- Exercise4_Laksup_S.py
+++ Exercise4_Laksup_S.py
@@ -3,7 +3,7 @@ gb = 74.5
 ics = 86.75
 cp = 90
 print("--- Your Score ---")
-print("Foundation English               :",fe)
-print("General Business                 :",gb)
-print("Introduction to Computer Systems :",ics)
-print("Computer Programming             :",cp)
+print("Foundation English :", fe)
+print("General Business :", gb)
+print("Introduction to Computer Systems :", ics)
+print("Computer Programming :", cp)
